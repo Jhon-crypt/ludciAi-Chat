4e028cf3ca48314cb2afafc0d658d5ababb3d0b7
jhon-crypt <johnoladele690@gmail.com>
2023-09-20 21:06:01 +0100

    Made soem changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,10 @@
-//This is running for the frontend without any api fetch
-///The api fetch is down below comments
 function displayText() {
+
     const inputText = document.getElementById('input-text').value;
+
     const displayArea = document.getElementById('display-area');
+
+
 
     if (inputText.trim() === '') {
         return; // Don't add empty messages
@@ -11,7 +13,7 @@ function displayText() {
     // Create a new message element
     const newMessageElement = document.createElement('div');
     newMessageElement.textContent = inputText;
-    newMessageElement.classList.add('message', 'fade-up-text', 'user-message');
+    newMessageElement.classList.add('flex', 'fade-up-text', 'user-message', 'justify-end', 'py-1', 'space-y-1', 'break-words', 'rounded-[10px]', 'bg-cream-600', 'p-3', 'ml-auto', 'w-fit', 'mb-3');
 
     // Clear the input field
     document.getElementById('input-text').value = '';
@@ -19,7 +21,7 @@ function displayText() {
     // Apply fade-in animation to the new message element
     setTimeout(function () {
         newMessageElement.style.opacity = 1;
-    }, 100); // 0.1-second delay for immediate fade-in
+    }, 10); // 0.1-second delay for immediate fade-in
 
     // Append the new message element to the display area
     displayArea.appendChild(newMessageElement);
@@ -27,27 +29,155 @@ function displayText() {
     // Remove old user messages if there are more than 2
     const userMessages = document.querySelectorAll('.user-message');
     if (userMessages.length > 1) {
-        displayArea.removeChild(userMessages[0]);
+        userMessages[0].classList.add('fade-out');
+
+        // Listen for the animation end event
+        userMessages[0].addEventListener('animationend', function () {
+            displayArea.removeChild(userMessages[0]);
+        });
     }
 
     // Set a default message to appear after the user sends a message
+
     setTimeout(function () {
         const defaultTextElement = document.createElement('div');
-        defaultTextElement.textContent = "Hahaha, now you're just messing with me! 🤣 You went from 'hi' to 'hi' to random letters to 'hello'! 🤪 Are you just seeing how far you can push my AI conversational skills? 😜";
-        defaultTextElement.classList.add('message', 'fade-up-text', 'other-message');
+
+
+        // Function to wrap every 6 words in a <p> tag
+        function wrapWordsInPTags(text, wordsPerParagraph = 6) {
+            // Split the text into words
+            const words = text.split(/\s+/);
+
+            // Initialize an empty array to store paragraphs
+            const paragraphs = [];
+
+            // Loop through the words and group them into paragraphs
+            for (let i = 0; i < words.length; i += wordsPerParagraph) {
+                const paragraphWords = words.slice(i, i + wordsPerParagraph);
+                const paragraph = `<span>${paragraphWords.join(' ')}</span> `;
+                paragraphs.push(paragraph);
+            }
+
+            // Join the paragraphs and wrap them in a container element (e.g., <div>)
+            const wrappedParagraphs = `<div class="py-1 mb-4 last:mb-0">${paragraphs.join('')}</div>`;
+
+            // Count the number of <span> elements generated
+            const spanCount = (wrappedParagraphs.match(/<span>/g) || []).length;
+
+            return { wrappedHTML: wrappedParagraphs, spanCount };
+        }
+
+        // Example usage
+        const inputText = "Hahaha! 😆 Now you're just being silly. 😜 I know you're capable of more than just hi! 😝 Come on, let's chat about something a bit more substantial! 😄 I promise I'll be an excellent conversationalist! 😁";
+        const { wrappedHTML, spanCount } = wrapWordsInPTags(inputText);
+        console.log(wrappedHTML);
+
+        // Assuming you have the spanCount value from the previous code
+
+
+        // Define the CSS animation properties
+        const animationDuration = '0.7s';
+        const animationDelayBase = '0.1s';
+        const animationTimingFunction = 'cubic-bezier(0.11, 0, 0.5, 0)';
+        const animationName = 'fade-in-word';
+
+        // Loop through each <span> and apply the CSS animation
+        for (let i = 0; i < spanCount; i++) {
+            const animationDelay = `${parseFloat(animationDelayBase) * i}s`;
+            const cssRule = `
+span:nth-child(${i + 1}) {
+animation: ${animationName} ${animationDuration} ${animationDelay} forwards ${animationTimingFunction};
+}
+`;
+
+            // Create a <style> element and append it to the <head>
+            const styleElement = document.createElement('style');
+            styleElement.textContent = cssRule;
+            document.head.appendChild(styleElement);
+        }
+
+
+
+        defaultTextElement.innerHTML = wrappedHTML;
+
+        defaultTextElement.classList.add('other-message', 'sentence');
         displayArea.appendChild(defaultTextElement);
+
+        // Get all sentence elements
+        const sentences = document.querySelectorAll('.ai-message');
+
+        // Function to trigger the animation for each sentence
+        function animateSentences() {
+            sentences.forEach((sentence, index) => {
+                // Apply a staggered delay to each sentence
+                setTimeout(() => {
+                    sentence.style.animationPlayState = 'running';
+                }, index * 500); // Adjust the delay duration (in milliseconds) as needed
+            });
+        }
+
+        animateSentences()
+
 
         // Remove old other messages if there are more than 2
         const otherMessages = document.querySelectorAll('.other-message');
         if (otherMessages.length > 1) {
-            displayArea.removeChild(otherMessages[0]);
+            otherMessages[0].classList.add('fade-out');
+
+            otherMessages[0].addEventListener('animationend', function () {
+                displayArea.removeChild(otherMessages[0]);
+            });
+            //displayArea.removeChild(otherMessages[0]);
         }
+
+
 
         setTimeout(function () {
             defaultTextElement.style.opacity = 1;
-        }, 100); // 0.1-second delay for immediate fade-in of default message
-    }, 1000); // 1-second delay after the user sends a message
+        }, 10); // 0.1-second delay for immediate fade-in of default message
+    }, 10); // 1-second delay after the user sends a message
+
 }
+
+const textarea = document.getElementById('input-text');
+const sendButton = document.getElementById('sendButton');
+
+sendButton.style.backgroundColor = '#F5EEDF';
+
+// Event listener for changes in the textarea content
+textarea.addEventListener('input', () => {
+    // Check if the textarea content is empty
+    if (textarea.value.trim() === '') {
+        // Set the button's background color to the default Tailwind CSS color
+        sendButton.style.backgroundColor = '#418A2F';
+        sendButton.style.backgroundColor = '#F5EEDF';
+    } else {
+        // Change the button's background color to the hover Tailwind CSS color
+        sendButton.style.backgroundColor = '#F5EEDF';
+        sendButton.style.backgroundColor = '#418A2F';
+    }
+});
+
+// Event listener for Enter key press within the textarea
+textarea.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault(); // Prevent the default behavior (line break)
+        displayText()
+    }
+});
+
+
+function scrollToTarget() {
+    const targetDiv = document.getElementById('scrollTarget');
+    if (targetDiv) {
+        targetDiv.scrollIntoView({ behavior: 'auto', block: 'nearest', inline: 'nearest' });
+    }
+}
+
+// Call the function when the page is loaded
+window.addEventListener('load', scrollToTarget);
+
+
 
 ///////////////////////////////////////////////////
 /*
